d758f36826c132d7cbc78d4b985ea6bcb57785ce
AlmirDNeto <almirdankar@yahoo.com.br>
2016-11-08 19:22:49 -0200

    enviando classe item

---
+++ projetoerin/src/projetoerin/Item.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetoerin;
+
+/**
+ *
+ * @author Laboratorio
+ */
+public class Item {
+    
+    private Integer numero;
+    private Integer quantidade;
+    private Double percDesconto;
+
+    /**
+     * @return the numero
+     */
+    public Integer getNumero() {
+        return numero;
+    }
+
+    /**
+     * @param numero the numero to set
+     */
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    /**
+     * @return the quantidade
+     */
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    /**
+     * @param quantidade the quantidade to set
+     */
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    /**
+     * @return the percDesconto
+     */
+    public Double getPercDesconto() {
+        return percDesconto;
+    }
+
+    /**
+     * @param percDesconto the percDesconto to set
+     */
+    public void setPercDesconto(Double percDesconto) {
+        this.percDesconto = percDesconto;
+    }
+    
+    
+}
